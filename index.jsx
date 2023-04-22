e3a5976f0b30d79d6e0946bc68e2af76ba4f0588
pavan1125 <pavanmedarametla9959@gmail.com>
2023-04-22 15:18:49 +0530

    added

--- index.jsx
+++ index.jsx
@@ -7,22 +7,28 @@ import Vans from "./pages/Vans"
 import VanDetail from "./pages/VanDetail"
 
 import "./server"
+import Layout from './Layout/Layout';
+import Dashboard from './pages/Host/DashBoard';
+import HostLayout from './Layout/HostLayout';
+import Income from './pages/Host/Income';
+import Reviews from './pages/Host/Reviews';
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
+
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
